71b0a2a985300b5ee5dc563d17f52bf03e3adc4f
Luca <luca@MacBook-Pro-di-openmind.local>
2017-04-20 13:25:24 +0200

    when a new animal is clicked, the previuosly clicked one get removed from the center of the screen

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 
 var moveAnimalOnClick = function (animal) {
     if (!$(animal).hasClass("selectedAnimal")) {
+        $(".selectedAnimal").removeClass("selectedAnimal"); // targer the current selected animal and unselect it so that they don't overlap
         $(animal).addClass("selectedAnimal");
     } else {
         $(animal).removeClass("selectedAnimal");
