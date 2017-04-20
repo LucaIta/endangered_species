019fd20a4885e9f0b604b68da995c7ac109c4633
Luca <luca@MacBook-Pro-di-openmind.local>
2017-04-20 13:16:07 +0200

    when clicked, animals get moved to the center of the oage

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
-$("document").ready(function () {
 
-})
+var moveAnimalOnClick = function (animal) {
+    if (!$(animal).hasClass("selectedAnimal")) {
+        $(animal).addClass("selectedAnimal");
+    } else {
+        $(animal).removeClass("selectedAnimal");
+    };
+};
+
+
+$("document").ready(function () {
+    $(".animal").click(function () {
+        moveAnimalOnClick(this);
+    });
+});
+
 
