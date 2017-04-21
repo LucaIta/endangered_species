4e37575bad457eec4b55bb5c5549263a0cc166de
Luca <luca@MacBook-Pro-di-openmind.local>
2017-04-21 14:36:47 +0200

    unselect animal when the background or masthead is clicked. Added style.

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,18 @@
 var moveAnimalOnClick = function (animal) {
     if (!$(animal).hasClass("selectedAnimal")) {
         $(".selectedAnimal").removeClass("selectedAnimal"); // target the current selected animal and unselect it so that they don't overlap
-        // $(animal).addClass("selectedAnimal");
     }
-        // } else {
-    //     $(animal).removeClass("selectedAnimal");
-    // };
     $(animal).toggleClass("selectedAnimal");
-    // $(animal).toggleClass("invisibleField");
 };
 
 
 $("document").ready(function () {
     $(".animal").click(function () {
         moveAnimalOnClick(this);
+    });
+    $(".worldMapContainer , .masthead").click(function () {
+        $(".selectedAnimal").removeClass("selectedAnimal"); // target the current selected animal and unselect it so that they don't overlap
+        // console.log("yeeee");
     });
 });
 
