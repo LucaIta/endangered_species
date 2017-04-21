3e3447a7efb340c9043a0bb78aa91c01171e7089
Luca <luca@MacBook-Pro-di-openmind.local>
2017-04-21 11:18:12 +0200

    Make selected animals, overlap the ones which have not been selected. It will add other styling to the animal divs and transition effects.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
 
 var moveAnimalOnClick = function (animal) {
     if (!$(animal).hasClass("selectedAnimal")) {
-        $(".selectedAnimal").removeClass("selectedAnimal"); // targer the current selected animal and unselect it so that they don't overlap
-        $(animal).addClass("selectedAnimal");
-    } else {
-        $(animal).removeClass("selectedAnimal");
-    };
+        $(".selectedAnimal").removeClass("selectedAnimal"); // target the current selected animal and unselect it so that they don't overlap
+        // $(animal).addClass("selectedAnimal");
+    }
+        // } else {
+    //     $(animal).removeClass("selectedAnimal");
+    // };
+    $(animal).toggleClass("selectedAnimal");
+    // $(animal).toggleClass("invisibleField");
 };
 
 
